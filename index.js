a0044f1a76e555cbbd241914a5806e4e31cee64e
Ravi Ranjan Yadav <66769928+ravi1104@users.noreply.github.com>
2024-01-18 13:01:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js
-
 import express from "express";
 import path from "path";
 import expressEjsLayouts from 'express-ejs-layouts';
@@ -9,7 +7,6 @@ import userRouter from "./src/features/routes/employee.router.js"
 import { viewAllEmployeesRepo } from "./src/features/repository/admin.repository.js";
 import { pendingReviewRepo } from "./src/features/repository/employee.repository.js";
 import { requestReviewRepo } from "./src/features/repository/performance.repository.js";
-
 const app = express();
 
 app.use(expressEjsLayouts);
@@ -22,42 +19,41 @@ app.use(session({
     secret: 'your-secret-key',
     resave: false,
     saveUninitialized: true
-}));
+  }));
 
-// Routes
+//Routes
 app.get('/', async (req, res) => {
     try {
-        let employees = [];
-        let pendingReview = [];
-        let showLogin = true;
+      let employees = [];
+      let pendingReview = [];
+      let showLogin = true;
 
-        if (req.session._id) {
-            employees = await viewAllEmployeesRepo(req.session._id);
-            pendingReview = await requestReviewRepo(req.session._id);
-            showLogin = false;
-            return res.render('home', { req, employees, pendingReview, showLogin, showlogout: true });
-        }
-
-        res.render('home', { req, employees, pendingReview, showLogin });
+      if (req.session._id) {
+        employees = await viewAllEmployeesRepo(req.session._id);
+        pendingReview = await requestReviewRepo(req.session._id);
+        showLogin = false;
+        return res.render('home', { req, employees, pendingReview, showLogin,showlogout:true });
+      }
+  
+      res.render('home', { req, employees, pendingReview, showLogin });
     } catch (error) {
-        res.status(500).send('Internal Server Error');
-        console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+      console.error('Error:', error);
     }
-});
-
-app.get('/login', (req, res) => {
-    res.render('login-register', { showlogin: true });
-});
-
-app.get('/register', (req, res) => {
-    res.render('login-register', { showlogin: false });
-});
-
-app.use('/admin', adminRouter);
-app.use('/user', userRouter);
-
+  });
+  
+app.get('/login',(req,res)=>{
+    res.render('login-register',{showlogin:true})
+})
+app.get('/register',(req,res)=>{
+    res.render('login-register',{showlogin:false});
+})
+app.use('/admin',adminRouter);
+app.use('/user',userRouter);
 app.get('*', (req, res) => {
-    res.render('error');
+    res.render('error')
 });
 
-export { app };  // Export the Express app
+
+
+export default app;
